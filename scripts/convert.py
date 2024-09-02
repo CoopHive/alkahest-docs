e6f6cbae99121a26049441ffac525546e4e2328b
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-09-02 22:36:47 +0800

    fix convert

--- scripts/convert.py
+++ scripts/convert.py
@@ -8,17 +8,13 @@ src_path = "../alkahest-obsidian"
 dest_path = "../alkahest-mdbook/src"
 
 files = [
-    f[len(src_path) + 1 :]
-    for f in glob.glob(f"{src_path}/**", recursive=True)
+    f[len(src_path) + 1 :] for f in glob.glob(f"{src_path}/**", recursive=True) 
     if ".obsidian" not in f and ".git" not in f
 ]
 
-
 def convert_obsidian_links(source, files):
     def get_relative_path(source_path, target_path):
-        return os.path.relpath(target_path, os.path.dirname(source_path)).replace(
-            "\\", "/"
-        )
+        return os.path.relpath(target_path, os.path.dirname(source_path)).replace("\\", "/")
 
     def find_matching_file(link, files):
         matching_files = [f for f in files if f.lower().endswith(f"/{link.lower()}.md")]
@@ -27,24 +23,19 @@ def convert_obsidian_links(source, files):
     def replace_link(match):
         link = match.group(1)
         mf = find_matching_file(link, files)
-        return (
-            f"[{link}]({get_relative_path(source, mf).replace(' ', '_')})"
-            if mf
-            else match.group(0)
-        )
-
+        return f"[{link}]({get_relative_path(source, mf).replace(' ', '_')})" if mf else match.group(0) 
+    
     def replace_image_link(match):
         image = match.group(1)
         image_path = get_relative_path(source, f"{src_path}/{image}")
         return f"![{image}]({image_path.replace(' ', '_')})"
-
+    
     with open(source, "r", encoding="utf-8") as file:
         content = file.read()
 
     content = re.sub(r"\[\[(.*?)\]\]", replace_link, content)
     content = re.sub(r"!\[\[(.*?)\]\]", replace_image_link, content)
     return content
-
 
 # content
 shutil.rmtree(dest_path, ignore_errors=True)
@@ -60,29 +51,24 @@ for f in files:
             )
             with open(f"{dest_path}/{f}".replace(" ", "_"), "w") as dest:
                 dest.write(content)
-    elif any(f.lower().endswith(ext) for ext in [".png", ".jpg", ".jpeg", ".gif"]):
+    elif any(f.lower().endswith(ext) for ext in ['.png', '.jpg', '.jpeg', '.gif']):
         # Copy image files
         dest_dir = f"{dest_path}/{os.path.dirname(f)}".replace(" ", "_")
         os.makedirs(dest_dir, exist_ok=True)
-        shutil.copy2(
-            f"{src_path}/{f}", f"{dest_dir}/{os.path.basename(f).replace(' ', '_')}"
-        )
-
+        shutil.copy2(f"{src_path}/{f}", f"{dest_dir}/{os.path.basename(f).replace(' ', '_')}")
 
 # summary
 def file_to_line(f):
     tabs = f.count("/") * "\t"
     title = f.split("/")[-1].split(".")[0]
     if f.endswith(".md"):
-        return f"{tabs}- [{title}](./{f.replace(' ', '_')})\n"
+        return f"{tabs}- [{title}](./{f.replace(" ", "_")})\n"
     else:
         return f"{tabs}- [{title}]()\n" if f + ".md" not in files else ""
 
-
 summary = "# Summary\n\n"
 for f in sorted(files[1:]):
-    if f.lower().endswith((".md", ".png", ".jpg", ".jpeg", ".gif")):
-        summary += file_to_line(f)
+    summary += file_to_line(f)
 
 with open(f"{dest_path}/SUMMARY.md", "w") as dest:
     dest.write(summary)
